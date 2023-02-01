25ea19daaa7cc57f53bec6ab8ac01c3252a7faa8
Akash Gautam <akashgautam@geekyants.com>
2023-02-01 11:07:00 +0530

    made a basic cube with stars bg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { OrbitControls, Stars } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <ambientLight color="pink" intensity={0.6} />
+      <directionalLight color="red" position={[-3, -2, -1]} />
+      <directionalLight color="red" position={[2, 3, 1]} />
+      <OrbitControls />
+      <Stars />
+      <mesh>
+        <boxGeometry attach={"geometry"}/>
+        <meshStandardMaterial attach={"material"}/>
+      </mesh>
+    </Canvas>
   );
 }
 
